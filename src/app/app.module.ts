bdd7d8ba3d377a1377d5b3c4df9d4348585b8d66
17Akshar <aksharparmar2002@gmail.com>
2023-04-11 18:44:12 +0530

    folder structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './client/login/login/login.component';
+import { HomeComponent } from './client/home/home/home.component';
+import { CartComponent } from './client/cart/cart/cart.component';
+import { BillsComponent } from './client/history/bills/bills.component';
+import { TransactionComponent } from './client/history/transaction/transaction.component';
+import { FeedbackComponent } from './client/feeback/feedback/feedback.component';
+import { PopUpComponent } from './client/pop-up/pop-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    CartComponent,
+    BillsComponent,
+    TransactionComponent,
+    FeedbackComponent,
+    PopUpComponent
   ],
   imports: [
     BrowserModule,
