1e17781205ab6ee161ef167bd1f4eef4104e4ed2
17Akshar <aksharparmar2002@gmail.com>
2023-04-25 17:38:58 +0530

    changes

--- src/app/client/home/home/home.component.ts
+++ src/app/client/home/home/home.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  isdown:boolean = false;
+  icons:any = "arrow_drop_down_circle"
+  flag:number = 1; //1 - 1st clicked ;0 - 2nd clicked
   constructor() { }
-
   ngOnInit(): void {
   }
-
+  items:any = ["fast","Beverages","Snacks"]
+  Down(ele:any,index:number){
+    // this.isdown = true;
+    console.log(index);
+    console.log(this.items[index]);
+    if(this.isdown == false){
+      this.isdown = true;
+      this.icons = "arrow_drop_up"
+    }
+    else{
+      this.isdown = false;
+      this.icons = "arrow_drop_down_circle"
+    }
+  }
 }
