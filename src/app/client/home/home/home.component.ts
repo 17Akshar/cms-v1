b4b17bfb7013dc4e4e13763de4a2a4638900e603
17Akshar <aksharparmar2002@gmail.com>
2023-04-25 15:17:07 +0530

    Home page done

--- src/app/client/home/home/home.component.ts
+++ src/app/client/home/home/home.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  IsOpen1:boolean = true
+  IsOpen2:boolean = false
+  IsOpen3:boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  Open(n:any){
+    if(n==1){
+      this.IsOpen1 =!this.IsOpen1
+      this.IsOpen2 = false
+      this.IsOpen3 = false
+    }
+    if(n==2){
+      this.IsOpen2 =!this.IsOpen2
+      this.IsOpen1 = false
+      this.IsOpen3 = false
+    }
+    if(n==3){
+      this.IsOpen3 =!this.IsOpen3
+      this.IsOpen2 = false;
+      this.IsOpen1 = false
+    }
 
+  }
 }
